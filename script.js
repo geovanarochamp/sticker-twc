0aab850360de03c79c767143da46d9d4033568d9
geovanarochamp <geovana.rocha.martins@gmail.com>
2022-10-26 10:27:35 -0300

    add flip card animation

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 isGreen = true
+isflip = false
 
 function changeCard() {
     const card = document.querySelector(".card")
     const footer = document.querySelector(".footer img")
+    const flipper = document.querySelector(".flipper")
+
     const backgroundImage = isGreen
         ? "url(./assets/bg-blue.svg)"
         : "url(./assets/bg-green.svg)"
+ 
+    const cardFlip = isflip
+        ? "rotateY(180deg) scaleX(-1)"
+        : "rotateY(0deg) scaleX(1)"
+    isflip = !isflip
+    flipper.style.transform = cardFlip
 
     const footerImage = isGreen
     ? "./assets/bg-blue-footer.svg"
@@ -13,4 +22,6 @@ function changeCard() {
     isGreen = !isGreen
     card.style.backgroundImage = backgroundImage
     footer.setAttribute('src', footerImage)
+
+    
 }
